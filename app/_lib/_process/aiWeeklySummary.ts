cb4ac43ab486f176e8817e9d023f0b14cca920e5
kevinccbsg <kevinccbsg@gmail.com>
2025-12-02 19:40:42 +0100

    fix: add better logs

--- app/_lib/_process/aiWeeklySummary.ts
+++ app/_lib/_process/aiWeeklySummary.ts
@@ -3,5 +3,6 @@ import { sendPromptMessage } from '@/app/_lib/_slack'
 
 export const aiWeeklySummary = async (channel: string, tools: string, prompt: string) => {
   const response = await processPrompt(tools, prompt)
+  console.log('sending slack message to channel', channel)
   await sendPromptMessage(channel, response);
 }
--- app/api/ai-weekly-summary/route.ts
+++ app/api/ai-weekly-summary/route.ts
@@ -34,4 +34,3 @@ export async function POST(request: Request) {
     return Response.json({ success: false }, { status: 500 })
   }
 }
-
